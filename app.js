5e98157ed71f13fa0711f61a418a3e6bfe8bb156
indra7777 <y21co021@rvrjc.ac.in>
2025-04-04 16:51:44 +0530

    removed bot toke

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   clientId: process.env.SLACK_CLIENT_ID,
   clientSecret: process.env.SLACK_CLIENT_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
+  // token: process.env.SLACK_BOT_TOKEN,
   // appToken: process.env.SLACK_APP_TOKEN,
   stateSecret: process.env.STATE_SECRET,
   scopes: manifest.oauth_config.scopes.bot,
